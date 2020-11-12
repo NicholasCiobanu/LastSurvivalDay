f0252813ccf80eccdc2b1ea68b8a38bee749cdec
Alvin Alagos <alagos_alvin@hotmail.com>
2020-11-11 19:20:10 -0500

    added zombie models, movement will be added later with animation

--- LastSurvivalDay/MyWorld.java
+++ LastSurvivalDay/MyWorld.java
@@ -8,7 +8,6 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class MyWorld extends World
 {
-
     /**
      * Constructor for objects of class MyWorld.
      * 
@@ -18,8 +17,49 @@ public class MyWorld extends World
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
         prepare();
+        int count = 0;
+        int spawnSpeed = 50;
+        int randomSpawn;
     }
-
+    public void act()
+    {
+      int count = 0;
+      int spawnSpeed = 50;
+      int randomSpawn;
+      if (count % spawnSpeed == 0)
+        {
+            randomSpawn = Greenfoot.getRandomNumber(8);
+            switch(randomSpawn){
+                case (0): 
+                addObject(new Zombies(), 0, 0);
+                break;
+                case(1):
+                addObject(new Zombies(), getWidth()/2, 0);
+                break;
+                case(2):
+                addObject(new Zombies(), 0, getHeight()/2);
+                break;
+                case(3):
+                addObject(new Zombies(), 0, getHeight()/3);
+                break;
+                case(4):
+                addObject(new Zombies(), getWidth()/4, getHeight());
+                break;
+                case(5):
+                addObject(new Zombies(), getWidth(), getHeight()/5);
+                break;
+                case(6):
+                addObject(new Zombies(), getWidth(), getHeight());
+                break;
+                case(7):
+                addObject(new Zombies(), getWidth()/3, 0);
+                break;
+                case(8):
+                addObject(new Zombies(), 0, getHeight()/2);
+                break;
+            }
+        }
+    }
     /**
      * Prepare the world for the start of the program.
      * That is: create the initial objects and add them to the world.
--- LastSurvivalDay/zombies.java
+++ LastSurvivalDay/zombies.java
@@ -1,19 +1,29 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class zombies here.
+ * Write a description of class Zombies here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class zombies extends Actor
+public class Zombies extends Actor
 {
+    int image = 0;
+    int count;
     /**
-     * Act - do whatever the zombies wants to do. This method is called whenever
+     * Act - do whatever the Zombies wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    public Zombies()
+    {
+        setImage("zombie.png");
+        
+    }
     public void act() 
     {
-        // Add your action code here.
+        count++;
+        
+        
     }    
+    
 }
