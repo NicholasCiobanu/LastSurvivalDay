591c56e5274f6d4c236dd162fda8c97561184efe
Alvin Alagos <alagos_alvin@hotmail.com>
2020-11-13 23:31:25 -0500

    added pursuit of player, more work to be done

--- LastSurvivalDay/MyWorld.java
+++ LastSurvivalDay/MyWorld.java
@@ -7,7 +7,9 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @version (a version number or a date)
  */
 public class MyWorld extends World
-{
+{   int spawnSpeed = 3;
+    int speedRate = 0;
+    public Character main = new Character();
     /**
      * Constructor for objects of class MyWorld.
      * 
@@ -15,50 +17,26 @@ public class MyWorld extends World
     public MyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
+        super(1000, 600, 1); 
+        addObject(main,325,180);
         prepare();
-        int count = 0;
-        int spawnSpeed = 50;
-        int randomSpawn;
+        spawn();
     }
     public void act()
     {
-      int count = 0;
-      int spawnSpeed = 50;
-      int randomSpawn;
-      if (count % spawnSpeed == 0)
-        {
-            randomSpawn = Greenfoot.getRandomNumber(8);
-            switch(randomSpawn){
-                case (0): 
-                addObject(new Zombies(), 0, 0);
-                break;
-                case(1):
-                addObject(new Zombies(), getWidth()/2, 0);
-                break;
-                case(2):
-                addObject(new Zombies(), 0, getHeight()/2);
-                break;
-                case(3):
-                addObject(new Zombies(), 0, getHeight()/3);
-                break;
-                case(4):
-                addObject(new Zombies(), getWidth()/4, getHeight());
-                break;
-                case(5):
-                addObject(new Zombies(), getWidth(), getHeight()/5);
-                break;
-                case(6):
-                addObject(new Zombies(), getWidth(), getHeight());
-                break;
-                case(7):
-                addObject(new Zombies(), getWidth()/3, 0);
-                break;
-                case(8):
-                addObject(new Zombies(), 0, getHeight()/2);
-                break;
-            }
-        }
+        
+    }
+    public void spawn()
+    {
+        int count = 0;
+      while(count <= 20)
+      {
+      if(Greenfoot.getRandomNumber(100)== 1)
+    {
+      addObject(new Zombies(main) , Greenfoot.getRandomNumber(1000), 400);
+      count++;
+    }
+    }
     }
     /**
      * Prepare the world for the start of the program.
@@ -66,7 +44,6 @@ public class MyWorld extends World
      */
     private void prepare()
     {
-        Character main = new Character();
-        addObject(main,325,180);
+     
     }
 }
--- LastSurvivalDay/zombies.java
+++ LastSurvivalDay/zombies.java
@@ -8,22 +8,45 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Zombies extends Actor
 {
-    int image = 0;
+    int switchImage = 0;
     int count;
+    private GreenfootImage image1 = null; 
+    private GreenfootImage image2 = null;
+    Character player;
     /**
      * Act - do whatever the Zombies wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
-    public Zombies()
+    public Zombies(Character main)
     {
-        setImage("zombie.png");
-        
+        player = main;
+        image1 = new GreenfootImage("zombie.png");
+        image2 =  new  GreenfootImage("zombie2.png");
+        setImage(image1);
+        setRotation(180);
     }
     public void act() 
     {
-        count++;
-        
-        
+       switchImage();
+       moveAround();
     }    
-    
+    public void switchImage()
+    {   
+        if(Greenfoot.getRandomNumber(10)== 1)
+        {
+        if (getImage() == image1)
+        {
+            setImage(image2);
+        }
+        else
+        {
+            setImage(image1);
+        }
+    }
+    }
+    public void moveAround()
+    {
+        move(Greenfoot.getRandomNumber(5));
+        turnTowards(player.getX(), player.getY());
+    }
 }
